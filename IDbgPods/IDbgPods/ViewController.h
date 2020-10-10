666be59ba27869fdf4d8002ab88aaef3d3eeabcf
mjzheng <mjzheng@tencent.com>
2020-10-10 17:40:08 +0800

    add files

---
+++ IDbgPods/IDbgPods/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  IDbgPods
+//
+//  Created by mjzheng on 2020/10/10.
+//  Copyright © 2020年 mjzheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+- (IBAction)onBtnClick:(id)sender;
+
+
+@end
+
---
+++ IDbgPods/IDbgPods/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  IDbgPods
+//
+//  Created by mjzheng on 2020/10/10.
+//  Copyright © 2020年 mjzheng. All rights reserved.
+//
+
+#import "ViewController.h"
+
+#import "MiniDumpFile.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (IBAction)onBtnClick:(id)sender {
+    MiniDumpFile* file = [[MiniDumpFile alloc] init];
+    NSString* dump = [file generateMiniDump];
+    NSLog(@"%@", dump);
+}
+
+
+@end

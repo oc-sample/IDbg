a87309b0e51dd3fcb594103554f4fabe36621fdf
mj9527 <mjsecurity@126.com>
2021-04-23 15:10:54 +0800

    update

---
+++ IDbgMac/IDbgMac/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  IDbgMac
+//
+//  Created by 郑俊明 on 2021/4/23.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ IDbgMac/IDbgMac/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  IDbgMac
+//
+//  Created by 郑俊明 on 2021/4/23.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+

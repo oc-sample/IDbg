188159ccd7a1dcc3b5da32df7c0b0b2d19642bf8
mjzheng <mjzheng@tencent.com>
2021-04-25 15:11:41 +0800

    update

--- IDbg/IDbg/sys_util.cc
+++ IDbg/IDbg/sys_util.cc
@@ -6,7 +6,21 @@
 //
 
 #include "sys_util.h"
+
+#if defined(OS_WIN)
+#include <windows.h>
+const DWORD kVCThreadNameException = 0x406D1388;
+#pragma pack(push, 8)
+typedef struct tagTHREADNAME_INFO {
+  DWORD dwType; // Must be 0x1000.
+  LPCSTR szName; // Pointer to name (in user addr space).
+  DWORD dwThreadID; // Thread ID (-1=caller thread).
+  DWORD dwFlags; // Reserved for future use, must be zero.
+} THREADNAME_INFO;
+#pragma pack(pop)
+#else
 #include <pthread.h>
+#endif
 
 void SetThreadName(const std::string& name) {
   if (name.empty()) {

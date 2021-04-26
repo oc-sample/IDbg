6c6116102de457394b693253420b51b82f3aaecf
mjzheng <mjzheng@tencent.com>
2021-04-26 14:16:45 +0800

    update

---
+++ app/ios/idbg_demo/idbg_demo/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  idbg_demo
+//
+//  Created by mjzheng on 2021/4/26.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ app/ios/idbg_demo/idbg_demo/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  idbg_demo
+//
+//  Created by mjzheng on 2021/4/26.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view.
+}
+
+
+@end

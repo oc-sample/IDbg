3924400123a81845ea598c4b58e818211e29a981
mjzheng <mjzheng@tencent.com>
2021-04-26 11:05:55 +0800

    update

---
+++ platforms/ios/CMakeFiles/3.18.4/CompilerIdCXX/CompilerIdCXX.build/Debug-iphoneos/CompilerIdCXX.build/Objects-normal/arm64/CMakeCXXCompilerId.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/mjzheng/Documents/mj_git/oc-sample/IDbg/platforms_project/ios/CMakeFiles/3.18.4/CompilerIdCXX/CMakeCXXCompilerId.cpp
---
+++ platforms/ios/IDbg.build/Debug-iphoneos/ZERO_CHECK.build/Script-57682B678D914897B178C6B9.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+make -C /Users/mjzheng/Documents/mj_git/oc-sample/IDbg/platforms_project/ios -f /Users/mjzheng/Documents/mj_git/oc-sample/IDbg/platforms_project/ios/CMakeScripts/ZERO_CHECK_cmakeRulesBuildPhase.make$CONFIGURATION OBJDIR=$(basename "$OBJECT_FILE_DIR_normal") all

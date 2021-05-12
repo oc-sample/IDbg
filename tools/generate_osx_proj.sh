6d83f22227120bee3d434ccdd30870148dddd6a8
mjzheng <mjzheng@tencent.com>
2021-05-12 17:04:00 +0800

    update

--- tools/generate_osx_proj.sh
+++ tools/generate_osx_proj.sh
@@ -8,3 +8,5 @@ current_dir=$(cd "$(dirname "$0")";pwd)
 #echo ${project_name}
 
 cmake .. -GXcode -B $current_dir/../project/osx -DCMAKE_TOOLCHAIN_FILE=$current_dir/ios.toolchain.cmake -DPLATFORM=MAC -DARCHS="arm64 x86_64" -DENABLE_BITCODE=0
+
+open ../project/osx/cross_idbg.xcodeproj/

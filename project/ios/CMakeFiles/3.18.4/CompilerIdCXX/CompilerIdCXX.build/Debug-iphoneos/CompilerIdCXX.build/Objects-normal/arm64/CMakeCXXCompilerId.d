058ce4690c3db54d069b9cf1f342a0f9c457f92b
mjzheng <mjzheng@tencent.com>
2021-05-12 16:26:31 +0800

    update

--- project/ios/CMakeFiles/3.18.4/CompilerIdCXX/CompilerIdCXX.build/Debug-iphoneos/CompilerIdCXX.build/Objects-normal/arm64/CMakeCXXCompilerId.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/mjzheng/Documents/mj_git/oc-sample/IDbg/project/ios/CMakeFiles/3.18.4/CompilerIdCXX/CMakeCXXCompilerId.cpp
--- project/ios/IDbg/version_config.h
+++
@@ -1,3 +0,0 @@
-// the configured options and settings for Tutorial
-#define base_VERSION_MAJOR 
-#define base_VERSION_MINOR 

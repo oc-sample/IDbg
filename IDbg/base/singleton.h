6ab40994b65db008fcdbd95cb98101b03fd35830
mjzheng <mjzheng@tencent.com>
2021-05-13 15:12:05 +0800

    update

--- IDbg/base/singleton.h
+++ IDbg/base/singleton.h
@@ -8,53 +8,34 @@
 #include <mutex>
 #include <thread>
 
-class Singleton {
-private:
-  Singleton() = default;  // 构造函数私有化
+namespace IDbg {
 
-  Singleton(const Singleton& other) =delete;  // 禁止拷贝构造函数
-
-  Singleton& operator=(const Singleton& other) = delete;  // 禁止赋值函数
-
-public:
-  ~Singleton() = default;
-
-  static Singleton* GetInstance() {
-    if (nullptr == m_singleton) {
-      std::lock_guard<std::mutex> lock(m_mutex);
-      if (nullptr == m_singleton) {
-        m_singleton = new Singleton();
-      }
-    }
-    return m_singleton;
-  }
-
-  static Singleton* GetInstance2() {
-    static Singleton instance;
-    return &instance;
-  }
-
-private:
-  static Singleton* m_singleton;
-  static std::mutex m_mutex;
+class NonCopyable {
+ protected:
+  virtual ~NonCopyable() = default;
+  NonCopyable() = default;
+ private:
+  NonCopyable(const NonCopyable& other) = delete;
+  NonCopyable(NonCopyable&& other) = delete;
+  NonCopyable& operator=(const NonCopyable& other) = delete;
+  NonCopyable& operator=(NonCopyable&& other) = delete;
 };
 
-Singleton* Singleton::m_singleton = nullptr;
-std::mutex Singleton::m_mutex;
+template <typename T>
+class Singleton : public NonCopyable {
+ public:
+  static T* Instance();
+ protected:
+  virtual ~Singleton() = default;
+  Singleton() = default;
+};
 
-void TestSingleton() {
-  Singleton* p1 = Singleton::GetInstance();
-  Singleton* p2 = Singleton::GetInstance();
-  if (p1 == p2) {
-    std::cout << "same" << std::endl;
-  }
-
-  Singleton* p3 = Singleton::GetInstance2();
-  Singleton* p4 = Singleton::GetInstance2();
-  if (p3 == p4) {
-    std::cout << "same" << std::endl;
-  }
+template <typename T>
+T * Singleton<T>::Instance() {
+  static T instance;
+  return &instance;
 }
 
+}  // namespace IDbg
 
 #endif //ALGORITHM_SINGLETON_H

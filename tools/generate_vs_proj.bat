679af710f7882bba23219dba53f6f14278dfc175
mjzheng <mjzheng@tencent.com>
2021-05-14 17:16:51 +0800

    update

--- tools/generate_vs_proj.bat
+++ tools/generate_vs_proj.bat
@@ -1,4 +1,4 @@
 set current_dir=%~dp0
 call .\init_git_pre_commit.bat
 cmake -G"Visual Studio 14 2015" %current_dir%\..\CMakeLists.txt -B%current_dir%\..\project\windows
-start ..\project\windows\cross_idgb.sln
+start ..\project\windows\cross_idbg.sln

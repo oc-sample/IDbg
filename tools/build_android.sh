76118db3c132e82bedc8d85881be477883c9f45d
mjzheng <mjzheng@tencent.com>
2021-05-17 10:10:57 +0800

    update

---
+++ tools/build_android.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cmake ../src \
+  -DCMAKE_SYSTEM_NAME=Android \
+  -DCMAKE_SYSTEM_VERSION=21 \
+  -DCMAKE_ANDROID_ARCH_ABI=arm64-v8a \
+  -DCMAKE_ANDROID_NDK=/Users/mjzheng/Library/Android/sdk/ndk/21.3.6528147 \

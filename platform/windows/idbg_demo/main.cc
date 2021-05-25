fd75fff65c31851fa0fadf774721885c1ba1f0fb
mjzheng <mjzheng@tencent.com>
2021-05-25 21:51:43 +0800

    add malloc

--- platform/windows/idbg_demo/main.cc
+++ platform/windows/idbg_demo/main.cc
@@ -4,8 +4,29 @@
 
 //#pragma comment ( lib, "third_party/IDbg/lib/IDbg.lib" )
 
+#include <Windows.h> 
+#include <iostream>
+
+#define COUNT 1000*1000
+void func() {
+  for (size_t i = 0; i < COUNT; ++i) {
+    int * pInt = (int*)malloc(i * sizeof(int));
+    free(pInt);
+  }
+}
+
+void TestFunc() {
+  DWORD tStart, tEnd;
+
+  tStart = timeGetTime();
+  func();
+  tEnd = timeGetTime();
+  printf("%lu\n", tEnd - tStart);
+}
+
 int main(int argc, char* argv[]) {
-  std::cout << "mjzheng" << std::endl;
+  TestFunc();
+  /*std::cout << "mjzheng" << std::endl;
 
   IDbg::SetThreadName("main");
 
@@ -14,7 +35,7 @@ int main(int argc, char* argv[]) {
     std::cout << "in thread model " << std::this_thread::get_id() << std::endl;
   });
 
-  std::this_thread::sleep_for(std::chrono::seconds(3));
+  std::this_thread::sleep_for(std::chrono::seconds(3));*/
 
   system("pause...");
 }
